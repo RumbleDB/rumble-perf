28eb122741606e92f9778b7acbeb5561d4db3521
Ghislain Fourny <gfourny@inf.ethz.ch>
2019-10-11 12:33:59 +0200

    Change memory to 2G.

--- rumble/measure-reddit.sh
+++ rumble/measure-reddit.sh
@@ -7,7 +7,7 @@ reps=32
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit --conf spark.dynamicAllocation.enabled=false --executor-cores 1 --executor-memory 1G --num-executors $i spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  spark-submit --conf spark.dynamicAllocation.enabled=false --executor-cores 1 --executor-memory 2G --num-executors $i spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
   echo $i >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
