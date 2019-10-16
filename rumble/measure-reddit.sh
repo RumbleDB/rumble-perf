2063609df72cdb147d44dd753ed5ef671889e47e
Ghislain Fourny <gfourny@inf.ethz.ch>
2019-10-16 10:45:43 +0200

    Update script.

--- rumble/measure-reddit.sh
+++ rumble/measure-reddit.sh
@@ -1,13 +1,69 @@
 #!/bin/bash
 
-reps=32
+reps=5
 
 > ./log-executors.txt
 
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit --conf spark.dynamicAllocation.enabled=false --executor-cores 1 --executor-memory 2G --num-executors $i spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
-  echo $i >> ./log-executors.txt
+  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-1.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  echo 1 >> ./log-executors.txt
+  cat ./log1-tmp.txt >> ./log-executors.txt
+done
+
+for i in `seq 1 $reps`;
+do
+  hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
+  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-2.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  echo 2 >> ./log-executors.txt
+  cat ./log1-tmp.txt >> ./log-executors.txt
+done
+
+for i in `seq 1 $reps`;
+do
+  hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
+  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-4.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  echo 4 >> ./log-executors.txt
+  cat ./log1-tmp.txt >> ./log-executors.txt
+done
+
+for i in `seq 1 $reps`;
+do
+  hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
+  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-8.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  echo 8 >> ./log-executors.txt
+  cat ./log1-tmp.txt >> ./log-executors.txt
+done
+
+for i in `seq 1 $reps`;
+do
+  hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
+  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-16.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  echo 16 >> ./log-executors.txt
+  cat ./log1-tmp.txt >> ./log-executors.txt
+done
+
+for i in `seq 1 $reps`;
+do
+  hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
+  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-32.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  echo 32 >> ./log-executors.txt
+  cat ./log1-tmp.txt >> ./log-executors.txt
+done
+
+for i in `seq 1 $reps`;
+do
+  hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
+  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-64.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  echo 64 >> ./log-executors.txt
+  cat ./log1-tmp.txt >> ./log-executors.txt
+done
+
+for i in `seq 1 $reps`;
+do
+  hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
+  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-all.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  echo 100 >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
