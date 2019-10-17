41409e3ca83b52ac88352ad842ceb8574b4efbeb
Ghislain Fourny <gfourny@inf.ethz.ch>
2019-10-17 16:42:36 +0200

    Modify allocation.

--- rumble/measure-reddit.sh
+++ rumble/measure-reddit.sh
@@ -7,7 +7,7 @@ reps=5
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-1.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  spark-submit --num-executors 126 --executor-cores 1 --executor-memory 2GB --conf spark.dynamicAllocation.enabled=false spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-1.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
   echo 1 >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
@@ -15,7 +15,7 @@ done
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-2.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  spark-submit --num-executors 126 --executor-cores 1 --executor-memory 2GB --conf spark.dynamicAllocation.enabled=false  spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-2.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
   echo 2 >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
@@ -23,7 +23,7 @@ done
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-4.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  spark-submit --num-executors 126 --executor-cores 1 --executor-memory 2GB --conf spark.dynamicAllocation.enabled=false  spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-4.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
   echo 4 >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
@@ -31,7 +31,7 @@ done
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-8.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  spark-submit --num-executors 126 --executor-cores 1 --executor-memory 2GB --conf spark.dynamicAllocation.enabled=false  spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-8.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
   echo 8 >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
@@ -39,7 +39,7 @@ done
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-16.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  spark-submit --num-executors 126 --executor-cores 1 --executor-memory 2GB --conf spark.dynamicAllocation.enabled=false  spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-16.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
   echo 16 >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
@@ -47,7 +47,7 @@ done
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-32.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  spark-submit --num-executors 126 --executor-cores 1 --executor-memory 2GB --conf spark.dynamicAllocation.enabled=false  spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-32.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
   echo 32 >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
@@ -55,7 +55,7 @@ done
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-64.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  spark-submit --num-executors 126 --executor-cores 1 --executor-memory 2GB --conf spark.dynamicAllocation.enabled=false  spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-64.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
   echo 64 >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
@@ -63,7 +63,7 @@ done
 for i in `seq 1 $reps`;
 do
   hadoop fs -rm -r hdfs:///user/hadoop/output-reddit
-  spark-submit spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-all.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
+  spark-submit --num-executors 126 --executor-cores 1 --executor-memory 2GB --conf spark.dynamicAllocation.enabled=false  spark-rumble-1.1-i.jar --query-path /home/hadoop/rumble-perf/rumble/reddit-filter-all.jq --output-path hdfs:///user/hadoop/output-reddit --log-path ./log1-tmp.txt
   echo 100 >> ./log-executors.txt
   cat ./log1-tmp.txt >> ./log-executors.txt
 done
